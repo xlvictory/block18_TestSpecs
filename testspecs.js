6f3b5aacb55a46935a707d3bd67bda951d149895
Xolani Victory <psymbiotic@xolanis-mbp.lan>
2023-11-22 11:43:45 -0500

    Completed Unit Testing portion

--- testspecs.js
+++ testspecs.js
@@ -1,3 +1,26 @@
 /* 
+UNIT TESTS:
+prompt 1: A function called "multiplication" that returns the product of the two input numbers.
 
+Considering (x,y) to be input integers:
+1. Expect multiply(x,y) to multiply the two integers
+2. Expect multiply(x,y) to output a number type
+3. Expect multiply(5, 12) to equal 60
+4. Expect multiply("four", 5) to return an error
+
+
+
+prompt 2: A function called "concatOdds" takes two arrays of integers as arguments. It should return a single array that only contains the odd numbers, in ascending order, from both of the arrays.
+
+1. Expect concatOdds() to concatenate two different arrays.
+2. Expect concatOdds() to return an array that only contains the odd numbers from each array.
+3. Expect concatOdds() to return the odd numbers array in ascending order.
+4. Expect concatOdds([1, 3 ,5 ,7 ,9], [1, 3, 2, 4, 7, 13, 21]) to return [1, 3, 5, 7, 9, 13, 21].
+5. Expect concatOdds() to take recurring numbers in the array, and return them as a single number (ex: two "1" values returning in the array as just "1").
+6. Expect concatOdds() to return an error if the user inputs strings or uses a string array.
+7. Expect concatOdds([2, 4, 6], [10, 20, 40]) to return an empty array/error, for they are even numbers.
+*/
+
+/* 
+FUNCTIONAL TESTS:
 */
